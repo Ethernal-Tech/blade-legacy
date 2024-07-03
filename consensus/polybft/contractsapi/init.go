0xPolygon/polygon-edge

Generate artifact

consensus/polybft/contractsapi/init.go
@@ -63,6 +63,9 @@ var (
 	ChildGovernor *artifact.Artifact
 	ChildTimelock *artifact.Artifact
 
+	// Account Abstraction
+	EntryPoint *artifact.Artifact
+
 	// test smart contracts
 	//go:embed test-contracts/*
 	testContracts          embed.FS
@@ -311,6 +314,11 @@ func init() {
 	}
 
 	ChildTimelock, err = artifact.DecodeArtifact([]byte(ChildTimelockArtifact))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	EntryPoint, err = artifact.DecodeArtifact([]byte(EntryPointArtifact))
 	if err != nil {
 		log.Fatal(err)
 	}

Ethernal-Tech/blade-legacy

consensus/polybft/contractsapi/init.go
@@ -64,6 +64,9 @@ var (
 	ChildGovernor *contracts.Artifact
 	ChildTimelock *contracts.Artifact
 
+	// Account Abstraction
+	EntryPoint *contracts.Artifact
+
 	// test smart contracts
 	//go:embed test-contracts/*
 	testContracts          embed.FS
@@ -338,6 +341,11 @@ func init() {
 	}
 
 	ZexNFT, err = contracts.DecodeArtifact(readTestContractContent("ZexNFT.json"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	EntryPoint, err = contracts.DecodeArtifact([]byte(EntryPointArtifact))
 	if err != nil {
 		log.Fatal(err)
 	}