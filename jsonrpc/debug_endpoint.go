645d467bfba4b9ef5d1fd1a94aed12a840c5f2ef
Novo Sandara <novosandara88@gmail.com>
2024-08-26 22:13:57 +0200

    cr fix

--- jsonrpc/debug_endpoint.go
+++ jsonrpc/debug_endpoint.go
@@ -245,7 +245,7 @@ func (d *Debug) GetRawHeader(filter BlockNumberOrHash) (interface{}, error) {
 		func() (interface{}, error) {
 			header, err := GetHeaderFromBlockNumberOrHash(filter, d.store)
 			if err != nil {
-				return nil, ErrHeaderNotFound
+				return nil, err
 			}
 
 			return header.MarshalRLP(), nil
